0310916dbf93dda7680eb9f07778cb683d734314
NOYBNGGA <166450538+NOYBNGGA@users.noreply.github.com>
2024-04-08 17:53:11 -0400

    Update Grabber.py

--- Grabber.py
+++ Grabber.py
@@ -9,13 +9,12 @@
 import requests
 import re
 import ctypes
-ctypes.windll.kernel32.SetConsoleTitleW("Coded by github.com/dev6920")
 
 print("Coded by dev github/dev6920")
 
 while True:
   
-  username = input("Enter username (type 'exit' to quit): ")
+  username = input("Mason_isbeast08 (type 'exit' to quit): ")
 
   if username == "exit":
     break
